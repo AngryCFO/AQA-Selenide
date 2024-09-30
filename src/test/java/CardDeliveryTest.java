fb892a65ffb8bba986aedade53d3c9511075f4ac
AngryCFO <a79955700980@gmail.com>
2024-09-30 03:18:20 +0300

    Updated

--- src/test/java/CardDeliveryTest.java
+++ src/test/java/CardDeliveryTest.java
@@ -1,13 +1,10 @@
-import com.codeborne.selenide.CollectionCondition;
 import com.codeborne.selenide.Condition;
-import com.codeborne.selenide.selector.ByText;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
 
 import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.Calendar;
 
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.*;
@@ -16,7 +13,10 @@ import static com.codeborne.selenide.Selenide.*;
 public class CardDeliveryTest {
     @Test
     void shouldTestFormPositive() {
+        // Открываем страницу
         open("http://localhost:9999/");
+
+        // Заполняем поля формы
         $("[data-test-id='city'] input").setValue("Санкт-Петербург");
         $("[data-test-id='date'] input").doubleClick().sendKeys(Keys.BACK_SPACE);
         String newDate = LocalDate.now().plusDays(3).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
@@ -24,12 +24,17 @@ public class CardDeliveryTest {
         $("[data-test-id='name'] input").setValue("Татьянина Татьяна");
         $("[data-test-id='phone'] input").setValue("+79001234567");
         $("[data-test-id='agreement']").click();
+
+        // Отправляем форму
         $(byText("Забронировать")).click();
+
+        // Проверяем, что сообщение об успехе отображается
         $(withText("успешно")).shouldBe(visible, Duration.ofSeconds(15));
     }
 
     @Test
     void shouldTestFormComplexCity() {
+        // Аналогично, тест для сложного города
         open("http://localhost:9999/");
         $("[data-test-id='city'] input").setValue("Мо").shouldBe(visible, Duration.ofSeconds(3));
         $(byText("Москва")).click();
@@ -45,6 +50,7 @@ public class CardDeliveryTest {
 
     @Test
     void shouldTestFormComplexDate() {
+        // Тест для сложной даты
         open("http://localhost:9999/");
         $("[data-test-id='city'] input").setValue("Казань");
         $("[data-test-id='date'] input").doubleClick().sendKeys(Keys.BACK_SPACE);
